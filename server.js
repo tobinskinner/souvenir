685a35c0c3996b87e97066c6a60914ab74f88d43
Josh Horner <josh.horner@gmail.com>
2013-08-17 12:02:52 -0400

    Adding Flickr (hardcoded for now)

--- server.js
+++ server.js
@@ -12,6 +12,37 @@ app.configure(function() {
 });
 
 // routes
+
+app.get('/flickr', function(req, res) {
+
+  // var user = req.query.user;
+  // var startdate = new Date(req.query.startdate);
+  // var enddate = new Date(req.query.enddate);
+  var user = '70625544%40N00';
+  var startdate = '1251000000000';
+  var enddate = '1251172799000';
+
+  var options = {
+    host: 'api.flickr.com',
+    path: '/services/rest/?method=flickr.photos.search' +
+      '&api_key=6ccf3ac4e38fcdc496798883300e8b6b' +
+      '&user_id=' + user +
+      '&min_taken_date=' + startdate +
+      '&max_taken_date=' + enddate +
+      '&format=json&nojsoncallback=1',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  rest.getJSON(options, function(statusCode, result) {
+    res.statusCode = statusCode;
+    res.send(result);
+  });
+
+});
+
 app.get('/twitpic/:user', function(req, res) {
 
   var user = req.params.user;
@@ -20,15 +51,17 @@ app.get('/twitpic/:user', function(req, res) {
 
   var results = [];
 
-  var getoptions = function(n) {
-
+  function getoptions(n) {
+    console.log('getoptions was called with: ' + n);
     var options = {
       host: 'api.twitpic.com',
       path: '/2/users/show.json?username=' + user + '?page=' + (n == 0 ? 0 : n),
       method: 'GET',
-      headers: {'Content-Type': 'application/json'}
+      headers: {
+        'Content-Type': 'application/json'
+      }
     };
-
+    console.log('we are returning: ' + JSON.stringify(options));
     return options;
 
   };
@@ -37,17 +70,24 @@ app.get('/twitpic/:user', function(req, res) {
 
     // number of pages to request in batches of 20 rounded up
     var pages = Math.ceil(result.photo_only_count / 20);
+    console.log('number of pages: ' + pages);
 
-    for (var i = 1; i <= pages; i++) {
-      rest.getJSON(getoptions(i), function(statusCode, nextresult) {
-        results.push(nextresult.images);
-      });
-    }
-
-    res.statusCode = statusCode;
-    res.send(results);
-
+    function repeater(p) {
+      if (p <= pages) {
+        console.log('start of if loop: ' + p);
+        rest.getJSON(getoptions(p), function(statusCode, nextresult) {
+          console.log('rest request ' + p + ' push to array');
+          results.push(nextresult.images);
+          console.log('current array ' + results);
+          repeater(p + 1);
+        });
+      }
+    };
+    repeater(1);
   });
+
+  res.statusCode = statusCode;
+  res.send(results);
 
 });
 

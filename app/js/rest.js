561ced377159aba1aaaeeb7875f9001c09ba0b98
Josh Horner <josh.horner@gmail.com>
2013-08-17 00:25:34 -0400

    Twitpic setup.

---
+++ app/js/rest.js
@@ -0,0 +1,26 @@
+var http = require('http');
+
+exports.getJSON = function(options, onResult) {
+
+    var req = http.request(options, function(res) {
+        var output = '';
+        console.log(options.host + ':' + res.statusCode);
+        res.setEncoding('utf8');
+
+        res.on('data', function(chunk) {
+            output += chunk;
+        });
+
+        res.on('end', function() {
+            var obj = JSON.parse(output);
+            onResult(res.statusCode, obj);
+        });
+    });
+
+    req.on('error', function(err) {
+        //res.send('error: ' + err.message);
+        console.log(err);
+    });
+
+    req.end();
+};
--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 var express = require('express'),
-  app = express();
+  app = express(),
+  rest = require('./app/js/rest.js');
 
 // configure Express
 app.configure(function() {
   app.use(express.favicon());
   app.use(express.static(__dirname + '/app'));
+  app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+});
+
+// routes
+app.get('/twitpic/:user', function(req, res) {
+
+  var user = req.params.user;
+
+  var options = {
+    host: 'api.twitpic.com',
+    // port: 443,
+    path: '/2/users/show.json?username=' + user,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  rest.getJSON(options, function(statusCode, result) {
+    console.log('onResult: (' + statusCode + ')' + JSON.stringify(result));
+    res.statusCode = statusCode;
+    res.send(result);
+  });
+
 });
 
 app.listen(9999);

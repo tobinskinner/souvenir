dc6b1a28d3cff549e1616927c5e446319d4f89c5
Tobin Skinner <tobin.skinner@gmail.com>
2013-08-17 00:28:02 -0400

    Updated controllers and continue to update UI.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,34 +5,54 @@
 angular.module('souvenirApp.controllers', [])
   .controller('souvenirAppController',
     function($scope) {
-      $scope.getFacebook = function(userInfoFacebook, dates) {
-        if (!dates || !dates.from || !dates.to) {
-          window.alert("Please enter your travel dates.");
-        } else if (!userInfoFacebook || !userInfoFacebook.username || !userInfoFacebook.password) {
-          window.alert("Please enter both a username and a password.");
-          return;
-        } else {
-          var dateFrom = new Date(dates.from);
-          var dateTo = new Date(dates.to);
-          if (dateFrom > dateTo) {
-            window.alert("Please verify dates.\nYour departure date is later than your returning date.");
-            return;
-          }
-
-          window.alert(JSON.stringify(userInfoFacebook) + " " + dates.from + " to " + dates.to);
-        }
-      };
+      // App-wide functions go here
     }
 )
 
-.controller('MyCtrl1',
-  function() {
+.controller('loginToSocialMediaController',
+  function($scope) {
+    // Login functions go here
+    $scope.getFacebook = function(userInfoFacebook, dates) {
+      if (!dates || !dates.from || !dates.to) {
+        window.alert("Please enter your travel dates.");
+      } else if (!userInfoFacebook || !userInfoFacebook.username || !userInfoFacebook.password) {
+        window.alert("Please enter both a username and a password for Facebook.");
+        return;
+      } else {
+        var dateFrom = new Date(dates.from);
+        var dateTo = new Date(dates.to);
+        if (dateFrom > dateTo) {
+          window.alert("Please verify dates.\nYour departure date is later than your returning date.");
+          return;
+        }
+
+        window.alert(JSON.stringify(userInfoFacebook) + " " + dates.from + " to " + dates.to);
+      }
+    };
+
+    $scope.getTwitter = function(userInfoFacebook, dates) {
+      if (!dates || !dates.from || !dates.to) {
+        window.alert("Please enter your travel dates.");
+      } else if (!userInfoFacebook || !userInfoFacebook.username || !userInfoFacebook.password) {
+        window.alert("Please enter both a username and a password for Twitter.");
+        return;
+      } else {
+        var dateFrom = new Date(dates.from);
+        var dateTo = new Date(dates.to);
+        if (dateFrom > dateTo) {
+          window.alert("Please verify dates.\nYour departure date is later than your returning date.");
+          return;
+        }
+
+        window.alert(JSON.stringify(userInfoFacebook) + " " + dates.from + " to " + dates.to);
+      }
+    };
 
   }
 )
 
-.controller('MyCtrl2',
-  function() {
-
+.controller('timelineController',
+  function($scope) {
+    // Timeline functions go here
   }
 );

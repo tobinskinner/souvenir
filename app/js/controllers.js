892f70d98cf59cd2627f4c2c34ea055118faca65
Tobin Skinner <tobin.skinner@gmail.com>
2013-08-17 15:40:16 -0400

    Fixed date problem.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -25,32 +25,43 @@ angular.module('souvenirApp.controllers', [])
       if (!dates || !dates.from || !dates.to) {
         window.alert("Please enter your travel dates.");
       } else {
-        var dateFrom = new Date(dates.from);
-        var dateTo = new Date(dates.to);
+        // Annoying workaround because javascript dates are awful
+        var dateFromArray = dates.from.split("-");
+        var dateToArray = dates.to.split("-");
+        var dateFromYear = dateFromArray[0];
+        var dateFromMonth = dateFromArray[1];
+        var dateFromDay = dateFromArray[2];
+        var dateToYear = dateToArray[0];
+        var dateToMonth = dateToArray[1];
+        var dateToDay = dateToArray[2];
+
+        var dateFrom = new Date(dateFromYear, (dateFromMonth - 1), dateFromDay);
+        var dateTo = new Date(dateToYear, (dateToMonth - 1), dateToDay);
+
+        console.log(dateFrom);
+        console.log(dateTo);
 
         if (dateFrom > dateTo) {
           window.alert("Please verify dates.\nYour departure date is later than your returning date.");
           return;
         }
 
-        var fromDateToString = JSON.stringify(dates.from);
-        var toDateToString = JSON.stringify(dates.to);
-        $scope.fromDate = fromDateToString;
-        $scope.toDate = toDateToString;
-        console.log(fromDateToString);
-        var currentDate = new Date(fromDateToString);
-        var finalDate = new Date(dates.to);
-        do {
-          $rootScope.media[currentDate.toLocaleDateString()] = {};
-          $rootScope.media[currentDate.toLocaleDateString()].date = currentDate.toDateString();
-          $rootScope.media[currentDate.toLocaleDateString()].flickr = [];
-          $rootScope.media[currentDate.toLocaleDateString()].twitter = [];
+        $scope.fromDate = dates.from;
+        $scope.toDate = dates.to;
 
-          currentDate.setDate(currentDate.getDate() + 1);
+        do {
+          $rootScope.media[dateFrom.toLocaleDateString()] = {};
+          $rootScope.media[dateFrom.toLocaleDateString()].date = dateFrom.toDateString();
+          $rootScope.media[dateFrom.toLocaleDateString()].flickr = [];
+          $rootScope.media[dateFrom.toLocaleDateString()].twitter = [];
+
+          dateFrom.setTime(dateFrom.getTime() + 1 * 86400000);
         }
-        while (currentDate.getDate() <= (finalDate.getDate() + 1));
+        while (dateFrom.getTime() <= dateTo.getTime());
         $scope.datesEntered = true;
       }
+
+      console.log($rootScope.media);
     };
 
     $scope.getFlickr = function(userInfoFlickr, dates) {
@@ -65,16 +76,25 @@ angular.module('souvenirApp.controllers', [])
         // TODO Change to REST API request
         // Add Flickr images to scope if they fall in date range
         Media.fakeFlickrData(userInfoFlickr.username, dateFrom, dateTo).then(function(flickrResponse) {
-          console.log(JSON.stringify(flickrResponse));
           for (var i = 0; i < flickrResponse.length; i++) {
             var url = "http://farm" + flickrResponse[i].farm + ".staticflickr.com/" + flickrResponse[i].server + "/" + flickrResponse[i].id + "_" + flickrResponse[i].secret + ".jpg";
-            console.log(url);
-
-            // var postedDate = new Date(flickrResponse[i].date * 1000).toLocaleDateString();
-            // if ($scope.media[postedDate]) {
-            //   $rootScope.media[postedDate].flickr.push(flickrResponse[i]);
-            // }
+            var postedDate = new Date(flickrResponse[i].datetaken).toLocaleDateString();
+            console.log(flickrResponse[i].datetaken);
+            console.log(postedDate);
+            if ($rootScope.media[postedDate]) {
+              var newFlickrObject = {};
+              if (flickrResponse[i].location) {
+                newFlickrObject.location = flickrResponse[i].location;
+              }
+              if (flickrResponse[i].title) {
+                newFlickrObject.title = flickrResponse[i].title;
+              }
+              newFlickrObject.imageURL = url;
+              $rootScope.media[postedDate].flickr.push(newFlickrObject);
+            }
           }
+          console.log($rootScope.media);
+
 
           $scope.loading = false;
           $scope.loggedInToFlickr = true;

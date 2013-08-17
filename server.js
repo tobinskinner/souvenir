c424fbcfa6e57fff38f831da92c7343ba0424696
Josh Horner <josh.horner@gmail.com>
2013-08-17 01:17:16 -0400

    More twitpic API stuff.

--- server.js
+++ server.js
@@ -15,21 +15,40 @@ app.configure(function() {
 app.get('/twitpic/:user', function(req, res) {
 
   var user = req.params.user;
+  var startdate = new Date(req.query.startdate);
+  var enddate = new Date(req.query.enddate);
 
-  var options = {
-    host: 'api.twitpic.com',
-    // port: 443,
-    path: '/2/users/show.json?username=' + user,
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
+  var results = [];
+
+  var getoptions = function(n) {
+
+    var options = {
+      host: 'api.twitpic.com',
+      path: '/2/users/show.json?username=' + user + '?page=' + (n == 0 ? 0 : n),
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
+    };
+    console.log(options);
+    return options;
+
   };
 
-  rest.getJSON(options, function(statusCode, result) {
-    console.log('onResult: (' + statusCode + ')' + JSON.stringify(result));
-    res.statusCode = statusCode;
-    res.send(result);
+  rest.getJSON(getoptions(0), function(statusCode, result) {
+
+    // number of pages to request in batches of 20 rounded up
+    var pages = Math.ceil(result.photo_only_count / 20);
+    console.log('photo: ' + result.photo_only_count);
+    console.log('pages: ' + pages);
+    for (var i = 1; i <= pages; i++) {
+
+      rest.getJSON(getoptions(i), function(statusCode, result) {
+        results.push(result.images);
+      });
+
+    }
+
+    res.send(results);
+
   });
 
 });

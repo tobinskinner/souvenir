5cdfba9f15ef5451ad6e426b74668a8198519b3f
Tobin Skinner <tobin.skinner@gmail.com>
2013-08-16 23:31:01 -0400

    Created wide scope controller for social media login info.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,37 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('souvenirApp.controllers', [])
+  .controller('souvenirAppController',
+    function($scope) {
+      $scope.getFacebook = function(userInfoFacebook, dates) {
+        if (!dates || !dates.from || !dates.to) {
+          window.alert("Please enter your travel dates.");
+        } else if (!userInfoFacebook || !userInfoFacebook.username || !userInfoFacebook.password) {
+          window.alert("Please enter both a username and a password.");
+          return;
+        } else {
+          var dateFrom = new Date(dates.from);
+          var dateTo = new Date(dates.to);
+          if (dateFrom > dateTo) {
+            window.alert("Please verify dates.\nYour departure date is later than your returning date.");
+            return;
+          }
 
-  }])
-  .controller('MyCtrl2', [function() {
+          window.alert(JSON.stringify(userInfoFacebook) + " " + dates.from + " to " + dates.to);
+        }
+      };
+    }
+)
 
-  }]);
+.controller('MyCtrl1',
+  function() {
+
+  }
+)
+
+.controller('MyCtrl2',
+  function() {
+
+  }
+);
